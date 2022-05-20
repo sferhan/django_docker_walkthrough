6c4530bab225092b014912ffc0381db38ddad312
Farhan Haider <sferhan@folio3.com>
2022-05-21 03:45:36 +0500

    created new endpoint for logs to demonstrate persisting storage inside container

--- django_hello_world/django_hello_world/views.py
+++ django_hello_world/django_hello_world/views.py
@@ -1,7 +1,19 @@
+from datetime import datetime
+from distutils.dir_util import mkpath
 
 from django.http import HttpResponse
 from django.conf import settings
 
+LOG_VOLUME_ROOT = '/log_data_volume'
+
 
 def index(req):
     return HttpResponse(f"<h1>Hello from {settings.SERVER_NAME}</h1>")
+
+
+def log(req):
+    mkpath(LOG_VOLUME_ROOT)
+    with open(f"{LOG_VOLUME_ROOT}/log.txt", "a+") as log_file:
+        log_file.write(f"Received log request at {datetime.now().isoformat()}\n")
+        log_file.seek(0)
+        return HttpResponse("</br>".join(log_file.readlines()))
